9846b2358482df7ffabd99e23a03fba5cdb327f5
Prabhu Gnana Sundar <pprabhugs@gmail.com>
2015-01-25 23:29:01 +0530

    Change Post and comment look and feel

--- mynote/notes/admin.py
+++ mynote/notes/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from notes.models import Post, Comment
+
+class ChoiceInline(admin.StackedInline):
+    model = Comment
+    extra = 0
+    fieldsets = [
+        # Uncomment this line to enable editing the comment
+        #(None, {'fields': ['comment_text']}),
+        ('Publish info', {'fields': ['commented_date'], 'classes': ['collapse']}),
+    ]
+
+class PostAdmin(admin.ModelAdmin):
+    #fields = ['publisheddate', 'posttext']
+
+    list_display = ('post_text', 'published_date')
+    list_filter = ['published_date']
+    search_fields = ['post_text']
+
+    fieldsets = [
+        (None, {'fields': ['post_text']}),
+        ('Publish info', {'fields': ['published_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
 
 # Register your models here.
+admin.site.register(Post, PostAdmin)
+#admin.site.register(Comment)
--- mynote/notes/models.py
+++ mynote/notes/models.py
@@ -6,10 +6,18 @@ from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
     def __str__(self):
-        return self.posttext
+        return self.post_text
 
-    posttext = models.CharField(max_length=500)
-    publisheddate = models.DateTimeField('date published')
+    post_text = models.CharField(max_length=500)
+    published_date = models.DateTimeField('published on')
 
     def waspublishedrecently(self):
-        return self.publisheddate >= timezone.datetime.now() - datetime.timedelta(days=1)
+        return self.published_date >= timezone.datetime.now() - datetime.timedelta(days=1)
+
+class Comment(models.Model):
+    def __str__(self):
+        return self.comment_text
+
+    post = models.ForeignKey(Post)
+    comment_text = models.CharField(max_length=500)
+    commented_date = models.DateTimeField('commented on')
